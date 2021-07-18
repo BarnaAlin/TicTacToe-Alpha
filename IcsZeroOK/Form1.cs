51c4f391e0ea0d3c55166c5f2155ab42396d6c74
BarnaAlin <barnaalin@yahoo.com>
2021-07-18 19:38:36 +0300

    Add project files.

---
+++ IcsZeroOK/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IcsZeroOK
+{
+    public partial class mainForm : System.Windows.Forms.Form
+    {
+        int currentUser;
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            InitGame();
+        }
+
+        private void InitGame()
+        {
+            CreateBoard();
+            currentUser = 1;
+        }
+
+        private void CreateBoard()
+        {
+            Console.WriteLine("aici cica se creaza matricea de 3x3");
+        }
+    }
+}
